0d956af96797ca3346f2036e145ec457eed40573
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-05-14 23:15:25 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,7 +51,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <p> Uncover the threads of creativity woven into each project, illustrating a dedication to pushing boundaries and delivering exceptional outcomes.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -83,7 +83,14 @@ export const Projects = () => {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <p>Tech stack used in Projects is : <br></br>
+                      MERN Stack<br></br>
+                      Natural Language Processing<br></br>
+                      Typescript<br></br>
+                      Python<br></br>
+                      C++,C,C#<br></br>
+                      Artifical Intelligence<br></br>
+                      Machine Learning</p>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
