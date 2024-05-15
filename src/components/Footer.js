2eabb559e03a6190b34613b8a3b2c8189e4071f0
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-05-15 21:40:53 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,8 +15,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/pranjali-bhardwaj-a33ba2294/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/PranjaliBhardwaj"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
           </Col>
